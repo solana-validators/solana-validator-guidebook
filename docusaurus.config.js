0f70a484f4aa099055388009ab0b966f6a82c1c1
tigarcia <tim.m.garcia@gmail.com>
2022-05-23 10:49:01 -0700

    Adding google analytics

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -32,6 +32,13 @@ const config = {
           customCss: require.resolve("./src/css/custom.css"),
         },
       }),
+      "@docusaurus/preset-classic",
+      {
+        gtag: {
+          trackingID: "G-03S61JX24N",
+          anonymizeIP: true,
+        },
+      },
     ],
   ],
 
