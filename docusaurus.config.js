8f76e5140a29e3fe79a43974d6bfacfd337c1553
tigarcia <tim.m.garcia@gmail.com>
2022-05-23 13:36:39 -0700

    Fixing gtag config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -31,14 +31,11 @@ const config = {
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
-      }),
-      "@docusaurus/preset-classic",
-      {
         gtag: {
           trackingID: "G-03S61JX24N",
           anonymizeIP: true,
         },
-      },
+      }),
     ],
   ],
 
